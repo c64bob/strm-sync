a0c7b43dbb0efb381fa854dd389ab2a0863007c2
c64bob <c64bob@users.noreply.github.com>
2020-09-08 19:19:24 +0200

    Update server.py

--- app/src/server.py
+++ app/src/server.py
@@ -1,10 +1,17 @@
+import os
+import scrapy
 from flask import Flask
 server = Flask(__name__)
 
 @server.route("/")
  def hello():
     # limit rate to prevent DOS style attacks
+   
     # crawl webpage and filter all desired file types
+    media_server = os.environ.get('MEDIA_SERVER')
+    #if media_server:
+     
+    
     # sync with output directory
     return "Hello World!"
 
