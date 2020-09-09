9a67bb5e9cc491d852352d18b6584c82a030bdba
c64bob <c64bob@users.noreply.github.com>
2020-09-09 09:56:46 +0200

    Update server.py

--- app/src/server.py
+++ app/src/server.py
@@ -1,15 +1,34 @@
 import os
+import re
 import scrapy
 from flask import Flask
 server = Flask(__name__)
+
+class MySpider(scrapy.Spider):
+    name = 'MEDIA_SERVER'
+    media_server = os.environ.get('MEDIA_SERVER')
+    media_regex = os.environ.get('MEDIA_REGEX')
+    start_urls = [ media_server ]
+    rules = (
+        # Extract links ending with a forward slash, and follow links from them
+        Rule(LinkExtractor(allow=('/$', ))),
+    )
+    def parse(self, response):
+        for href in response.xpath('//a/@href').getall():
+            match = re.search(media_regex, href)
+            if match:
+                yield href
+
+        for href in response.xpath('//a/@href').getall():
+            yield scrapy.Request(response.urljoin(href), self.parse)
+
+
 
 @server.route("/")
  def hello():
     # limit rate to prevent DOS style attacks
    
     # crawl webpage and filter all desired file types
-    media_server = os.environ.get('MEDIA_SERVER')
-    #if media_server:
      
     
     # sync with output directory
