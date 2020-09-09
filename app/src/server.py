0095f99057ea435471619f4f494706085941c0dc
c64bob <c64bob@users.noreply.github.com>
2020-09-09 11:39:09 +0200

    Update server.py

--- app/src/server.py
+++ app/src/server.py
@@ -3,12 +3,19 @@ import re
 import requests
 import xmltodict
 from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 server = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["1 per hour"]
+)
 
 @server.route("/")
+@limiter.limit("1 per hour")
  def hello():
-    # limit rate to prevent DOS style attacks
-   
     # crawl webpage and filter all desired file types
     media_server = os.environ.get('MEDIA_SERVER')
     media_regex = os.environ.get('MEDIA_REGEX')
