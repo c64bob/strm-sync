b7652b30ce30b59a1227c810d26944456690c790
c64bob <c64bob@users.noreply.github.com>
2020-09-09 11:04:35 +0200

    Update server.py

--- app/src/server.py
+++ app/src/server.py
@@ -1,35 +1,28 @@
 import os
 import re
-import scrapy
+import requests
+import xmltodict
 from flask import Flask
 server = Flask(__name__)
-
-class MySpider(scrapy.Spider):
-    name = 'MEDIA_SERVER'
-    media_server = os.environ.get('MEDIA_SERVER')
-    media_regex = os.environ.get('MEDIA_REGEX')
-    start_urls = [ media_server ]
-    rules = (
-        # Extract links ending with a forward slash, and follow links from them
-        Rule(LinkExtractor(allow=('/$', ))),
-    )
-    def parse(self, response):
-        for href in response.xpath('//a/@href').getall():
-            match = re.search(media_regex, href)
-            if match:
-                yield href
-
-        for href in response.xpath('//a/@href').getall():
-            yield scrapy.Request(response.urljoin(href), self.parse)
-
-
 
 @server.route("/")
  def hello():
     # limit rate to prevent DOS style attacks
    
     # crawl webpage and filter all desired file types
-     
+    media_server = os.environ.get('MEDIA_SERVER')
+    media_regex = os.environ.get('MEDIA_REGEX')
+    webdav_options = """<?xml version="1.0"?>
+                        <a:propfind xmlns:a="DAV:">
+                        <a:prop><a:resourcetype/></a:prop>
+                        </a:propfind>"""
+    headers = {'Depth': 'infinity'}
+    r = requests.request('PROPFIND', media_server, data=webdav_options, headers=headers)
+    xml_dict = xmltodict.parse(r.text, dict_constructor=dict)
+
+    for response in xml_dict['d:multistatus']['d:response']:
+        filelink = unquote(response['d:href'])
+        print(filelink)
     
     # sync with output directory
     return "Hello World!"
